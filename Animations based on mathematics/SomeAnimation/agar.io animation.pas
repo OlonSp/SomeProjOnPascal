17fcbcf4e9caa41c91f6c7e5b80d6c30a0eaf3b3
Pavel <cravt5@mail.ru>
2024-08-22 17:46:10 +0300

    Add some more intersting proj

---
+++ Animations based on mathematics/SomeAnimation/agar.io animation.pas
@@ -0,0 +1,44 @@
+﻿uses WPFObjects;
+
+begin
+  var (w,h) := (800,600);
+  Window.SetSize(w,h);
+  Window.CenterOnScreen;
+  loop 50 do
+  begin
+    var a := new CircleWPF(Random(50, w-50), Random(50, h-50),
+      Random(10.0,50.0), CLRandom);
+    (a.Dx, a.Dy) := Random2(-3.0, 3.0);
+  end;
+  while True do
+  begin
+     foreach var a in Objects do
+     begin
+       
+       if a.Left <= 0 then
+         a.Dx := abs(a.Dx)
+       else if a.Left + a.Width >= Window.Width then
+         a.Dx := -abs(a.Dx);
+       if a.Top <= 0 then
+         a.Dy := abs(a.Dy)
+       else if a.Top + a.Height >= Window.Height then
+         a.Dy := -abs(a.Dy);
+       
+       a.Move;
+       
+       foreach var b in a.IntersectionList do
+         if a.Width < b.Width then
+          begin
+            a.Dx := 100; b.Width += 4; b.Left -= 2; b.Top -= 2;
+          end
+         else
+         begin
+           b.Dx := 100; a.Width += 4; a.Left -= 2; a.Top -= 2;
+         end;
+     end;
+     for var i := Objects.Count - 1 downto 0 do
+       if Objects[i].Dx = 100 then
+         Objects[i].Destroy;
+     Sleep(10);
+  end;
+end.
---
+++ Kaleidoscope/Kaleidoscope.pas
@@ -0,0 +1,30 @@
+﻿uses GraphABC;
+
+begin
+  var w := 200;
+  Window.SetSize(2 * w, 2 * w);
+  Window.CenterOnScreen;
+  var p := new Picture(w, w);
+  Pen.Color := clTransparent;
+  while True do
+  begin
+    p.Clear;
+    for var i := 1 to 300 do
+    begin
+      Brush.Color := ARGB(Random(100, 200),
+        Random(256), Random(256), Random(256));
+      var a := Random(0, 359);
+      p.Pie(Random(0, w-1), Random(0, w-1), Random(10, 70), 
+        a, a + Random(20, 90));
+    end;
+    p.Draw(0,0);
+    p.FlipVertical;
+    p.Draw(0,w);
+    p.FlipHorizontal;
+    p.Draw(w,w);
+    p.FlipVertical;
+    p.Draw(w,0);
+    Window.Save(Random(0,9) + '.png');
+    Sleep(1000);
+  end;
+end.
